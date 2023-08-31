644dd9b223913a6ab3cd1ac3a25d52b9a707cd18
sako977 <sandeshkoirala@gmail.com>
2023-08-31 11:14:34 +0100

    first draft

--- VersionUpdater/Program.cs
+++ VersionUpdater/Program.cs
@@ -4,7 +4,37 @@
    {
       static void Main(string[] args)
       {
-         Console.WriteLine("Hello, World!");
+         try
+         {
+            ProcessLogic();
+         }
+         catch (Exception ex)
+         {
+            Console.WriteLine(ex.Message);
+            ProcessLogic(); // Exit only on success or user exit. 
+         }
+      }
+
+      private static void ProcessLogic()
+      {
+         Console.WriteLine("Select release type:\n" +
+         "1. Feature\n" +
+         "2. Bug Fix");
+
+         ConsoleKeyInfo keyInfo = Console.ReadKey();
+
+         Console.WriteLine("\nEnter an absolute file path");
+
+         string? softwareVersion = Console.ReadLine();
+         if (string.IsNullOrWhiteSpace(softwareVersion))
+         {
+            Console.WriteLine("\nLooking for 'ProductInfo.cs' in current directory.");
+            softwareVersion = Path.Combine(System.IO.Directory.GetCurrentDirectory(), "ProductInfo.cs"); ;
+         }
+
+
+         Versioning versioning = new Versioning(keyInfo, softwareVersion);
+         versioning.VersionIncrement();
       }
    }
 }
---
+++ VersionUpdater/Versioning.cs
@@ -0,0 +1,44 @@
+﻿namespace VersionUpdater
+{
+   public class Versioning
+   {
+      ConsoleKeyInfo consoleKeyInfo;
+      string version;
+      public Versioning(ConsoleKeyInfo consoleKeyInfo, string version)
+      {
+         this.consoleKeyInfo = consoleKeyInfo;
+         this.version = version;
+      }
+
+      public string VersionIncrement()
+      {
+         string[] releaseType = version.Split('.');
+
+         if (consoleKeyInfo.Key == ConsoleKey.NumPad0)
+         {
+            return Feature(releaseType);
+         }
+         else if (consoleKeyInfo.Key == ConsoleKey.NumPad2)
+         {
+            return BugFix(releaseType);
+         }
+
+         throw new Exception("Couldn't determine release type.");
+      }
+      private string Feature(string[] major)
+      {
+         int intValue = Convert.ToInt32(major[2]);
+         intValue++;
+         major[2] = intValue.ToString();
+         return string.Join('.', major);
+      }
+
+      private string BugFix(string[] minor)
+      {
+         int intValue = Convert.ToInt32(minor[3]);
+         intValue++;
+         minor[3] = intValue.ToString();
+         return string.Join('.', minor);
+      }
+   }
+}
